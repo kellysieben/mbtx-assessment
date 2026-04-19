d2d0cfb99d54f9f49010157b71c4f684fd9b26a5
kellysieben <kellysieben@gmail.com>
2026-04-19 01:25:14 +0000

    added CORS config

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContextFactory<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=mbtx.db"));
 
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? [];
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials());
+});
+
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<IRegisteredClientStore, RegisteredClientStore>();
 builder.Services.AddSingleton<SensorReadingStore>();
@@ -21,6 +31,8 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
+
+app.UseCors();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
